8b14ce97a155cfe2993750475aeebe46d0ede17d
naoki-tomita <ripvanvanwinkle@ezweb.ne.jp>
2016-04-09 15:17:00 +0900

    Initial Commit

---
+++ Looper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Looper
+//
+//  Created by 冨田 直希 on 2016/04/09.
+//  Copyright © 2016年 冨田 直希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
